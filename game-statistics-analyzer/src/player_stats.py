1efbb21e4cfd14bc95a615c3a94c0aa86927f793
chessguy7777 <jerry2007.xu@gmail.com>
2026-08-14 12:02:26 -0400

    First commit

---
+++ game-statistics-analyzer/src/player_stats.py
@@ -0,0 +1,16 @@
+player = [
+    {
+        "name": "Jerry",
+        "kills": 14,
+        "deaths": 12,
+        "accuracy": 78
+    },
+    {
+        "name": "Tom",
+        "kills": 10,
+        "deaths": 15,
+        "accuracy": 65
+    }
+]
+
+print(player[1]["kills"])
